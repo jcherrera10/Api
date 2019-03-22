fa2796ea0315acec8d5d08474588874340b0e1c0
jherrera10 <juan.herrera.barrios@gmail.com>
2019-03-22 13:40:22 -0500

    Post exercise

---
+++ ApiTestProject/Post.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTestProject
+{
+    class Request {
+        public static string MyFirstPost()
+        {
+
+            string result;
+            HttpWebResponse response;
+            JObject responseBody;
+            string payload = null;
+            string APIurl = "https://httpbin.org/get?nombre=Alejandro&age=32";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(APIurl);
+            request.Method = "POST";
+            request.AuthenticationLevel = System.Net.Security.AuthenticationLevel.MutualAuthRequested;
+            request.Credentials = CredentialCache.DefaultNetworkCredentials;
+            request.ContentType = "application/json; charset=utf-8";
+
+            Debug.WriteLine("Attempting to call TFS Rest API: " + APIurl);
+
+            try
+            {
+                 if (request.Method == "PATCH" || request.Method == "POST" || request.Method == "PUT")
+                 {
+                     //https://httpbin.org/post
+                     using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+                     {
+                         Debug.WriteLine("...Attempting to build Post/Patch request");
+                         streamWriter.Write(payload);
+                     }
+                 }
+                using (response = (HttpWebResponse)request.GetResponse())
+                {
+
+                    using (StreamReader rdr = new StreamReader(response.GetResponseStream()))
+                    {
+                        result = rdr.ReadToEnd();
+                        responseBody = JObject.Parse(result);
+                        int age = (int)responseBody["args"]["age"];
+
+
+                        Debug.WriteLine(result);
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                HttpWebResponse res = (HttpWebResponse)e.Response;
+
+                using (StreamReader rdr = new StreamReader(res.GetResponseStream()))
+                {
+                    result = rdr.ReadToEnd();
+                }
+
+                responseBody = JObject.Parse(result);
+                string errorMessage = responseBody.ToString();
+
+                Assert.Fail(errorMessage);
+            }
+
+            return result;
+        }
+    }   
+}
---
+++ ApiTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ApiTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ApiTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85725701-35ba-45bd-bb09-dba7f15e9b87")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
